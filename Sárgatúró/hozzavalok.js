57427b521adab6e7a7290269dae3bdf2f7c44c47
Toth Akos <toth.akos.ferenc24725@gmail.com>
2023-03-17 09:24:02 +0100

    files

---
+++ Sárgatúró/hozzavalok.js
@@ -0,0 +1,6 @@
+function recept() { 
+        let i = document.getElementById("fo").value; 
+        document.getElementById("tej").innerText = i / 5;
+        document.getElementById("tojas").innerText = i * 2; 
+        document.getElementById("cukor").innerText = i * 1
+}
